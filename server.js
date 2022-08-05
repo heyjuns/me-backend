0c8fef2782522112f0f7afe04715af4954a029a9
Juanda <heyjuns@icloud.com>
2022-08-05 22:29:21 +0700

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const data = require('./data.json')
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.json({ data })
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is Running on Port ${PORT}`)
+});
